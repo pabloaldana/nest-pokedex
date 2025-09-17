42b3da30360bd15793e53345d963f0c96ed4d0eb
Pablo Aldana <pabloaldana.cipo@gmail.com>
2025-09-17 11:56:51 -0300

    seccion 7

---
+++ src/common/pipes/parse-mongo-id.pipe.ts
@@ -0,0 +1,23 @@
+import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+import { isValidObjectId } from 'mongoose';
+import { throwError } from 'rxjs';
+
+@Injectable()
+export class ParseMongoIdPipe implements PipeTransform {
+
+
+
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    // console.log({value, metadata});
+
+    if (!isValidObjectId(value)) throw new BadRequestException(`"${value}" is not a valid MongoId`)
+
+    return value;
+  }
+
+
+
+
+
+}
